21aca1889f7160e28d2c4deff31cc31da2505e28
Thijs Koerselman <thijskoerselman@gmail.com>
2025-01-23 10:14:13 +0100

    Make it pure esm

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["esm", "cjs"],
+  entry: {
+    index: "src/index.ts",
+  },
+  format: "esm",
   target: "node20",
   sourcemap: true,
 });
