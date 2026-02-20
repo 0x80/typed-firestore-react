c4ee4d076b9443909f1973c905ed2460f6b5d852
Thijs Koerselman <thijskoerselman@gmail.com>
2026-02-20 17:44:34 +0100

    Add getDocuments and getDocumentsData functions

---
+++ src/get-documents.ts
@@ -0,0 +1,36 @@
+import {
+  getDocs,
+  query,
+  type CollectionReference,
+  type DocumentData,
+  type QueryConstraint,
+} from "firebase/firestore";
+import { makeMutableDocument } from "./make-mutable-document";
+
+export async function getDocuments<T extends DocumentData>(
+  collectionRef: CollectionReference<T>,
+  ...queryConstraints: QueryConstraint[]
+) {
+  const _query =
+    queryConstraints.length === 0
+      ? collectionRef
+      : query(collectionRef, ...queryConstraints);
+
+  const snapshot = await getDocs(_query);
+
+  return snapshot.docs.map((doc) => makeMutableDocument(doc));
+}
+
+export async function getDocumentsData<T extends DocumentData>(
+  collectionRef: CollectionReference<T>,
+  ...queryConstraints: QueryConstraint[]
+) {
+  const _query =
+    queryConstraints.length === 0
+      ? collectionRef
+      : query(collectionRef, ...queryConstraints);
+
+  const snapshot = await getDocs(_query);
+
+  return snapshot.docs.map((doc) => doc.data());
+}
